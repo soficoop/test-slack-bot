c55c7aa3576af18d8091b0abdcafb7e811ac8b8b
Lujain <lujain@sofi.coop>
2022-05-22 12:30:15 +0300

    auth

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ require("dotenv").config();
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  developerMode: true,
-  socketMode:false, // enable the following to use socket mode
-//   appToken: process.env.APP_TOKEN
+  // developerMode: true,
+  // socketMode:false, // enable the following to use socket mode
+  // appToken: process.env.APP_TOKEN
 });
 
-app.command("/test", async ({ command, ack, say }) => {
-    try {
-      await ack();
-      say("Yaaay! that command works!");
-    } catch (error) {
-      console.log("err the error is >>>>")
-      console.error(error);
-    }
-});
+// app.command("/test", async ({ command, ack, say }) => {
+//     try {
+//       await ack();
+//       say("Yaaay! that command works!");
+//     } catch (error) {
+//       console.log("err the error is >>>>")
+//       console.error(error);
+//     }
+// });
 
 (async () => {
     const port = 3010
