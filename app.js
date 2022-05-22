ae5c4f76c13a201b2b5fb2b7ad4e7c66e0cf9839
Lujain <lujain@sofi.coop>
2022-05-22 15:03:03 +0300

    .

--- app.js
+++ app.js
@@ -5,19 +5,29 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   // developerMode: true,
-  // socketMode:false, // enable the following to use socket mode
-  // appToken: process.env.APP_TOKEN
+  socketMode:true, // enable the following to use socket mode
+  appToken: process.env.APP_TOKEN
 });
 
-// app.command("/test", async ({ command, ack, say }) => {
-//     try {
-//       await ack();
-//       say("Yaaay! that command works!");
-//     } catch (error) {
-//       console.log("err the error is >>>>")
-//       console.error(error);
-//     }
-// });
+app.command("/test", async ({ command, ack, say }) => {
+    try {
+      await ack();
+      say("Yaaay! that command works!");
+    } catch (error) {
+      console.log("err the error is >>>>")
+      console.error(error);
+    }
+});
+
+app.command("/test1", async ({ command, ack, say }) => {
+  try {
+    await ack();
+    say("Yaaay! test1 command works!");
+  } catch (error) {
+    console.log("err the error is >>>>")
+    console.error(error);
+  }
+});
 
 (async () => {
     const port = 3010
